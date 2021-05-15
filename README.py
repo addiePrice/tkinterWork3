a08693ad9b49eb6b0290149b6d03b688440a92ab
addiePrice <79869276+addiePrice@users.noreply.github.com>
2021-05-14 21:11:35 -0600

    Update README.py

--- README.py
+++ README.py
@@ -21,16 +21,16 @@ def clearWindow():
 
 def mainMenu():
     clearWindow()
-    LMain = Label(top, text = "Block 5 GUIs", bg = "#b2d8d8")
+    LMain = Label(top, text = "Block 5 GUIs", bg = "#b2d8d8", font = ("Times", 11, "bold"))
     LMain.grid(column = 2, row = 1)
     
-    B1Main = Button(text = "Week 1", bg = "#66b2b2", command = week1)
+    B1Main = Button(text = "Week 1", bg = "#66b2b2", font = ("Times", 11), command = week1)
     B1Main.grid(column = 2, row = 2)
     
-    B2Main = Button(text = "Week 2", bg = "#008080", command = week2)
+    B2Main = Button(text = "Week 2", bg = "#008080", font = ("Times", 11), command = week2)
     B2Main.grid(column = 2, row = 3)
     
-    B3Main = Button(text = "Week 3", bg = "#006666")
+    B3Main = Button(text = "Week 3", bg = "#006666", font = ("Times", 11))
     B3Main.grid(column = 2, row = 4)
 
 def week1():
@@ -43,24 +43,24 @@ def week1():
     clearWindow()
     
     #this is a Label widget
-    L1 = Label(top, text = "Type stuff :)", bg = "#b2d8d8")
-    L1.grid(column = 0, row = 1)
+    L1 = Label(top, text = "Type stuff :)", bg = "#b2d8d8", font = ("Times", 11, "bold"))
+    L1.grid(column = 0, row = 2)
 
     #this is an Entry widget
     E1 = Entry(top, bd = 5)
-    E1.grid(column = 0, row = 2)
+    E1.grid(column = 0, row = 3)
 
     #this is a Button widget
-    B1 = Button(text = "   Print Stuff   ", bg = "#008080", command = results)
-    B1.grid(column = 0, row = 3)
+    B1 = Button(text = "   Print Stuff   ", bg = "#008080", font = ("Times", 11), command = results)
+    B1.grid(column = 0, row = 4)
 
-    B2 = Button(text = "  +  ", bg = "#66b2b2", command = addToList)
-    B2.grid(column = 1, row = 2)
+    B2 = Button(text = "  +  ", bg = "#66b2b2", font = ("Times", 11), command = addToList)
+    B2.grid(column = 1, row = 3)
 
-    B3 = Button(text = "  Export Stuff  ", bg = "#006666", command = exportList)
-    B3.grid(column = 0, row = 4)
+    B3 = Button(text = "  Export Stuff  ", bg = "#006666", font = ("Times", 11), command = exportList)
+    B3.grid(column = 0, row = 5)
     
-    Bclear = Button(text = "Main Menu", bg = "white", command = mainMenu)
+    Bclear = Button(text = "Main Menu", bg = "#ccece6", font = ("Times", 11), command = mainMenu)
     Bclear.grid(column = 3, row = 1)
 
 def week2():
@@ -72,22 +72,22 @@ def week2():
         for x in range(0, int(rollTimes)):
             myRolls.append(random.randint(1, int(dieType)))
 
-        L4W2 = Label(top, text = "Here are your results:", bg = "#b2d8d8")
+        L4W2 = Label(top, text = "Here are your results:", bg = "#b2d8d8", font = ("Times", 11))
         L4W2.grid(column = 2, row = 2)
-        L5W2 = Label(top, text = "{}".format(myRolls), bg = "#66b2b2")
+        L5W2 = Label(top, text = "{}".format(myRolls), bg = "#66b2b2", font = ("Times", 11))
         L5W2.grid(column = 2, row = 3)
-        B2W2 = Button(text = "Main Menu", bg = "white", command = mainMenu)
+        B2W2 = Button(text = "Main Menu", bg = "#ccece6", font = ("Times", 11), command = mainMenu)
         B2W2.grid(column = 3, row = 0)
 
         
     clearWindow()
-    L1W2 = Label(top, text="Dice Roller Program", bg = "#b2d8d8")
+    L1W2 = Label(top, text="Dice Roller Program", bg = "#b2d8d8", font = ("Times", 11, "bold"))
     L1W2.grid(column = 0, row = 1)
 
-    L2W2 = Label(top, text = "How many sides?", bg = "#66b2b2")
+    L2W2 = Label(top, text = "How many sides?", bg = "#66b2b2", font = ("Times", 11))
     L2W2.grid(column = 0, row = 2)
     
-    L3W2 = Label(top, text = "How many rolls?", bg = "#66b2b2")
+    L3W2 = Label(top, text = "How many rolls?", bg = "#66b2b2", font = ("Times", 11))
     L3W2.grid(column = 2, row = 2)
     
     E1W2 = Entry(top, bd = 5)
@@ -96,10 +96,9 @@ def week2():
     E2W2 = Entry(top, bd = 5)
     E2W2.grid(column = 2, row = 3)
     
-    B1W2 = Button(text = "Roll!", bg= "#008080", command = rollDice)
+    B1W2 = Button(text = "Roll!", bg= "#008080", font = ("Times", 11), command = rollDice)
     B1W2.grid(column = 2, row = 4)
 
 if __name__ == "__main__":
     mainMenu()
     top.mainloop()
-
